376b441f4cebdd9a878ebbd4dcfd13acd7e2112f
CIX Online Ltd <steve@icuk.net>
2017-11-30 13:58:41 +0000

    Update copyright and CIX name

--- moderate/bldinfo.h
+++ moderate/bldinfo.h
@@ -1,6 +1,6 @@
 #define	MAIN_PROGRAM_32				"MODERA32.ADN\0"
 #define	MAIN_PROGRAM				"MODERATE.ADN\0"
-#define	COPYRIGHT_STRING			"Copyright © CIX Online Ltd 1993-2014\0"
+#define	COPYRIGHT_STRING			"Copyright © CIX Ltd 1993-2017\0"
 #ifdef EVAL_VERSION
 #define	PRODUCT_NAME				"Moderator Addon for Ameol and Ameol2\0"
 #else
@@ -11,6 +11,6 @@
 #define	ADDON_VERSION_MINIMA		20
 #define	ADDON_VERSION_REVISION		00
 #define	PRODUCT_VERSION				"2.20.00\0"   
-#define	TRADEMARK_STRING			"Ameol(TM) is a trademark of CIX Online Ltd\0"
+#define	TRADEMARK_STRING			"Ameol(TM) is a trademark of CIX Ltd\0"
 #define	COMPANY_NAME				"CIX\0"
 
